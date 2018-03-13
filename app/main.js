0bf1b6c8e31dcac98ecc102c342e453a4ffb2c2c
jasenmichael <jasen@ready-builder.com>
2018-03-13 13:27:01 -0600

    create main.js, add people_one array

---
+++ app/main.js
@@ -0,0 +1,12 @@
+// Slack your partner. You should both create an array of objects. Each object should have a name and age.
+
+var people_one = [
+  {
+    name:  "Jasen",
+    age: "42"
+  },
+  {
+    name: "Jasper",
+    age: "7"
+  }
+]
